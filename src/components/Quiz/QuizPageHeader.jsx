ee6d937f89055df8e3397cea45d7c5d3cd1b5f74
programmeranamul <programmer.anamul@gmail.com>
2022-08-07 20:50:10 +0600

    second revision complete

--- src/components/Quiz/QuizPageHeader.jsx
+++ src/components/Quiz/QuizPageHeader.jsx
@@ -8,9 +8,9 @@ function QuizPageHeader({ score, quesIndex, totalQus }) {
     <div className="container">
       <h1 className="title-1 text-center mb-3 mb-sm-4">Lavel’s Quiz</h1>
       <div className="scoar-board bg-2">
-        <div className="">
+        <div className="position-relative">
         <h3 className="title-3 color-2 text-center">Scor {score}</h3>
-        <p className="fs-15 color-2 text-center">
+        <p className="fs-15 color-2 text-center qut">
           Question {quesIndex + 1}/{totalQus}
         </p>
         </div>
